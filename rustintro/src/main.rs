e233bdc51c2e99373db7f610c7c538b4c13a5139
Anastasiia Boiko <nastya.boiko@scrumlaunch.com>
2025-12-24 16:02:02 +0100

    feat: update main function and add utility functions in rustintro

--- rustintro/src/main.rs
+++ rustintro/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, from the main.rs file!");
+    get_height(180);
+    human_identifier("Nastya", 39, 168.1);
+}
+
+fn get_height(height: i32) -> f64 {
+    println!("Getting height: {}", height);
+  return height as f64;
+}
+
+fn human_identifier(name: &str, age: u32, height: f32) {
+   println!("My name is {}, and I am {} years old and {} cm tall", name, age, height);
 }

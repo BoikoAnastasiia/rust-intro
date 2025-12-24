6f5a38d9b6329785c069376cd081a13fc89243f7
Anastasiia Boiko <nastya.boiko@scrumlaunch.com>
2025-12-24 16:51:25 +0100

    fix: correct height in human_identifier and add BMI calculation

--- rustintro/src/main.rs
+++ rustintro/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
     println!("Hello, from the main.rs file!");
     get_height(180);
-    human_identifier("Nastya", 39, 168.1);
+    human_identifier("Nastya", 39, 168.6);
+    let x = {
+        let price : i32 = 10;
+        let tax : i32 = 2;
+        price + tax
+    };
+    println!("{}", x);
+    let added = add(1, 2);
+    println!("addition is {}", added);
+    let bmi = claculate_BMI(61.0, 1.68);
+    println!("BMI is {:.2}", bmi);
 }
 
 fn get_height(height: i32) -> f64 {
@@ -11,4 +21,14 @@ fn get_height(height: i32) -> f64 {
 
 fn human_identifier(name: &str, age: u32, height: f32) {
    println!("My name is {}, and I am {} years old and {} cm tall", name, age, height);
+}
+
+const X : i32 = 10;
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+const fn claculate_BMI(weight: f32, height: f32) -> f32 {
+    weight / (height * height)
 }
